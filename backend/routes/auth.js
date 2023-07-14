437a8f509f49a2dc5c759e62496b9d5b5fcafbaf
Shiva <shiva.khatri0001@gmail.com>
2023-07-14 11:12:55 -0400

    Added login endpoint, sent auth token on login, conditions for authorization...

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -65,4 +65,45 @@ router.post(
   }
 );
 
+
+//create a User using: POST "/api/auth/login" it doesn't require login
+router.post(
+    "/login",
+    [
+      
+      body("email", "Enter a valid email").isEmail(),
+      body("password", "password can't be blank").exists()
+     
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+          return res.status(400).json({ errors: errors.array() });
+        }
+        const {email, password} = req.body;
+        try{
+            let user = await User.findOne({email});
+            if(!user){
+                return res.status(400).json({error: "please try to login with correct credentials"});
+            }
+            const passwordCompare = await bcrypt.compare(password, user.password);
+            if(!passwordCompare){
+                return res.status(400).json({error: "please try to login with correct credentials"});
+            }
+            const data = {
+                user:{
+                    id:user.id
+                }
+              }
+              const authToken = jwt.sign(data, JWT_SECRET);
+              res.json({authToken})
+        }catch(error)  {
+            console.log(error.message);
+      res.status(500).send("Internal server occured");
+
+        }
+
+
+    })
+
 module.exports = router;
