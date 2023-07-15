6bbc92c49333b07620f4c3727439411b1e6cdae7
Shiva <shiva.khatri0001@gmail.com>
2023-07-15 15:00:52 -0400

    created end points to update the notes

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -52,4 +52,49 @@ router.post(
   }
 );
 
+
+//Route2 : updating an new note
+
+router.put("/updatenote/:id",fetchuser,
+[
+    body("title", "Enter a valid title").isLength({ min: 3 }),
+    body("description", "Description must be 5 characters.").isLength({
+      min: 5,
+    }),
+  ],
+async(req, res) =>{
+    try {
+        const{title, description, tag} = req.body;
+        // create a newNote object
+        const newNote = {};
+        if(title){ newNote.title = title};
+        if(description){ newNote.description = description};
+        if(tag){ newNote.tag = tag};
+
+        // Find the note to be  updated and update it
+
+        let note = await Note.findById(req.params.id);
+        if(!note){ return res.status(404).send("Not Found")};
+
+        if(note.user.toString() !== req.user.id){
+            return res.status(401).send("Not Allowed");
+        }
+
+        note = await Note.findByIdAndUpdate(req.params.id, {$set: newNote}, {new:true});
+        res.json({note});
+           
+            
+            
+
+        
+    } catch (error) {
+        console.log(error.message);
+      res.status(500).send("Internal server occured");
+        
+    }
+
+}
+);
+
+
 module.exports = router;
